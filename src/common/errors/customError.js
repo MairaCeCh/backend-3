6f02a34c9ecd3f0e7acaf895e6e734327d3cb22f
mayra <112666191+MairaCeCh@users.noreply.github.com>
2025-03-28 18:45:47 -0300

    agregando clase 4

---
+++ src/common/errors/customError.js
@@ -0,0 +1,26 @@
+import { json } from "express";
+import { logger } from "../utils/logger.js"
+
+
+
+export const customError = async (err, req, res, next)=>{
+
+    const statusCode = err.statusCode || 500;
+    const message = statusCode === 500 ? "Internal server error" : err.message;
+    const stack = err.stack.split("\n")
+    const error = {
+        statusCode,
+        files: stack,
+        message: err.message,
+        path: req.originalUrl,
+        method: req.method
+    }
+    
+  
+  
+  logger.log ("error", `status:${statusCode} [${req.method}] ${req.originalUrl} - msg:${err.message}`);
+  
+  console.log(error);
+  
+  res.status(statusCode).json({ statusCode, message });
+  }
---
+++ src/modules/pets/pet.service.js
@@ -0,0 +1,31 @@
+import { NotFoundError } from "../../common/errors/errors.js";
+import { generatePetsMocks } from "../../mock/pets.mocks.js";
+import { petDao } from "./pet.dao.js";
+
+class PetService{
+  async create(data){
+
+
+    
+    throw new NotFoundError();
+  }
+  
+  async getOne(query){
+    const pet = await petDao.getOne(query);
+    if(!pet) throw new NotFoundError("pet not found");
+
+    return pet;
+  }
+  async createPetsMocks(amount) {
+    const pets = generatePetsMocks(amount);
+    await petDao.removeAll();
+    for(const pet of pets){
+      await petDao.create(pet);
+    }
+
+    return pets;
+  }
+
+}
+
+export const petService = new PetService();
